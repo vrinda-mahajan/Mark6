3df01aebbfb43a1d5d7f377b00802d48a4e2d205
Vrinda Mahajan <vrindamahajan96@gmail.com>
2021-08-29 00:52:29 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
+// var serverURL =" https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationURL(text) {
+  return serverURL + "?text=" + text;
+}
+function errorHandler(error) {
+  console.log("error occured", error);
+  alert("Something wrong with server,Try Again Later!");
+}
+function handleClick() {
+  var inputText = txtInput.value;
+  fetch(getTranslationURL(inputText))
+    .then((response) => response.json())
+    .then((json) => (outputDiv.innerText = json.contents.translated))
+    .catch(errorHandler);
+}
+
+btnTranslate.addEventListener("click", handleClick);
